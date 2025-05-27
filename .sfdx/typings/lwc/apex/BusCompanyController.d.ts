5b124713f09ee8991363dc0ab2d04b0fa82f2854
Rimantas <rimantas.razmys@glamox.com>
2025-05-27 21:51:27 +0300

    first commit

---
+++ .sfdx/typings/lwc/apex/BusCompanyController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/BusCompanyController.getBusesWithPassengers" {
+  export default function getBusesWithPassengers(param: {companyId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/BusCompanyController.updateBusStatuses" {
+  export default function updateBusStatuses(param: {companyId: any}): Promise<any>;
+}
